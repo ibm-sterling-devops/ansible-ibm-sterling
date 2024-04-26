ecd5335875f2e77242c2f827fbb2d74462e8c4c0
ebasso <ebasso@ebasso.net>
2024-04-26 10:38:31 -0300

    github actions

---
+++ tools/site/copy-role-docs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SRC_DIR=$GITHUB_WORKSPACE/roles
+TO_DIR=$GITHUB_WORKSPACE/docs/roles
+
+mkdir -p $TO_DIR
+
+function copyDoc() {
+  ROLE=$1
+  cp $SRC_DIR/$ROLE/README.md $TO_DIR/$ROLE.md
+}
+
+copyDoc cd_deploy
+copyDoc sb2bi_deploy
